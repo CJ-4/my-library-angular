0b590462ca7920f8bb2b22d01ef732a2b960168c
CJ Forth <cforth@gmail.com>
2020-01-15 19:53:03 -0600

    "book class added"

---
+++ src/app/book.ts
@@ -0,0 +1,12 @@
+export class Book {
+    title: string;
+    author: string;
+    year: number;
+
+    constructor(title: string, category: string, year: number) {
+        this.title = title;
+        this.author = category;
+        this.year = year;
+    }
+}
+
